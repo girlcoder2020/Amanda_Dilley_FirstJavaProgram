3e82d1aebbf99ffaf64ffcd831a5463cdc24cd2b
Amanda <48845910+girlcoder2020@users.noreply.github.com>
2019-06-13 21:44:35 -0500

    added .java file
    completed first Java code write, added .java file and .class file

---
+++ LifeInfo.java
@@ -0,0 +1,7 @@
+public class LifeInfo {
+    public static void main(String[] args) {
+       System.out.println("My name is Amanda." + "\n"
+       +" My age is 37." + "\n"
+       +" My hometown is Garland, TX.");
+   }
+}
